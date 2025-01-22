cb4b1f59c88c003203f50f3362e7e7278d23c416
Mark Horvath <horvath.mark1@students.jedlik.eu>
2025-01-22 11:25:21 +0100

    new routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -6,34 +6,7 @@ const routes: RouteRecordRaw[] = [
     component: () => import('../layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('../pages/IndexPage.vue') }],
   },
-  {
-    path: '/xempty',
-    component: () => import('../layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('../pages/xEmptyPage.vue') }],
-  },
-  {
-    path: '/xtable',
-    component: () => import('../layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('../pages/xTablePage.vue') }],
-  },
-  {
-    path: '/xcarousel',
-    component: () => import('../layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('../pages/xCarouselPage.vue') }],
-  },
-  {
-    path: '/xcard',
-    component: () => import('../layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('../pages/xCardPage.vue') }],
-  },
-  {
-    path: '/xhelp',
-    component: () => import('../layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('../pages/xHelpPage.vue') }],
-  },
 
-  // Always leave this as last one,
-  // but you can also remove it
   {
     path: '/:catchAll(.*)*',
     component: () => import('../pages/ErrorNotFoundPage.vue'),
